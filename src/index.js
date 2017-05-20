4d397fef0bc06cfc2b352080d3afab26bbf629f4
yogieputra8 <yogie.putra8@gmail.com>
2017-05-21 00:57:27 +0700

    Our first Route definition

--- src/index.js
+++ src/index.js
@@ -7,25 +7,18 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/app';
 import reducers from './reducers';
 
+import PostsIndex from './components/posts_index';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 
-
-class Hello extends React.Component {
-  render(){ return <div>Hello!</div> }
-}
-
-class Goodbye extends React.Component {
-  render(){ return <div>Goodbye!</div> }
-}
 
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/hello" component={Hello} />
-        <Route path="/goodbye" component={Goodbye} />
+        <Route path="/" component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
