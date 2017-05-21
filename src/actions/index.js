7f850f0630c178d893c5367b652b85a04a5ee964
yogieputra8 <yogie.putra8@gmail.com>
2017-05-21 14:25:12 +0700

    Create post action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=lkwietpw';
@@ -11,6 +12,15 @@ export function fetchPosts() {
 
   return{
     type: FETCH_POSTS,
+    payload: request
+  }
+}
+
+export function createPost(values){
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return{
+    type: CREATE_POST,
     payload: request
   }
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/index';
+
 
 
 class PostsNew extends Component {
@@ -25,7 +28,8 @@ class PostsNew extends Component {
 
 
   onSubmit(values){
-    console.log(values)
+    // console.log(values)
+    this.props.createPost(values)
   }
 
 
@@ -79,4 +83,6 @@ function validate(values){
 export default reduxForm({
   validate, // validate: validate
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null,{ createPost })(PostsNew)
+);
