ab57f96ccbe850ff60f9f1fa0f898d3aa494c393
yogieputra8 <yogie.putra8@gmail.com>
2017-05-21 16:00:25 +0700

    make it Request API only once when show all posts then caching them

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { Link } from 'react-router-dom';
+
 
 class PostsShow extends Component{
   componentDidMount(){
-    const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    if(!this.props.post){
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
   }
 
   render(){
@@ -17,6 +21,9 @@ class PostsShow extends Component{
 
     return(
       <div>
+        <br/>
+        <Link to="/">Back to Index</Link>
+        <br/>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
